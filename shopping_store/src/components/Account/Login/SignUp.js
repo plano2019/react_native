42939c3b1e89c41938d32f6ec678a878367998da
banhmisg9509 <banhmisg9509@gmail.com>
2019-06-22 10:31:19 +0700

    change class name SignUp component

--- shopping_store/src/components/Account/Login/SignUp.js
+++ shopping_store/src/components/Account/Login/SignUp.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { View, TextInput, Text, TouchableOpacity, StyleSheet } from 'react-native';
 
-export class SignIn extends Component {
+export class SignUp extends Component {
      constructor(props) {
         super(props);
         this.state = {
