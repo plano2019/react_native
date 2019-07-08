d00b86a32ea63275ad11d21abdec0728ddefd158
Tran Ngoc Nha <tnnha@TMA.COM.VN>
2019-07-08 14:04:55 +0700

    retrieve firebase data for category

--- shopping_store/src/components/Home/Category.js
+++ shopping_store/src/components/Home/Category.js
@@ -2,25 +2,65 @@ import React, {Component} from 'react';
 import { Text, View, Image, StyleSheet, Dimensions } from 'react-native';
 import Swiper from 'react-native-swiper';
 
-import bannerImage from '../../assets/images/categories/fruit.jpg'
-import bo from '../../assets/images/categories/bo.png'
-import cam from '../../assets/images/categories/cam.jpg'
+import { firebaseApp } from '../../services/FirebaseConfig';
 
 const { width, height } = Dimensions.get('window');
 
+
 export default class Category extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            categories : [],
+            selectedIndex: 0
+        }
+    }
+
+    componentDidMount() {
+        firebaseApp.database().ref('categories').once('value', (snapshot) => {
+            let data = snapshot.val();
+            let items = Object.values(data);
+            this.setState({
+                categories: items
+            });
+        });
+    }
+
     render() {
-        const { wrapper, textStyle, imageStyle, imageStyleBo, imageStyleCam } = styles;
+        const { wrapper, textStyle, imageStyle, categoryTitle } = styles;
+        const {categories} = this.state;
         return (
             <View style={wrapper}>
                 <View style={{flex: 1, justifyContent: 'center'}}>
                     <Text style={textStyle}>Category</Text>
                 </View>
                 <View style={{flex: 4}}>
-                    <Swiper showsButtons showsPagination={false}>
-                        <Image source={bannerImage} style={imageStyle} />
-                        <Image source={bo} style={imageStyleBo} />
-                        <Image source={cam} style={imageStyleCam} />
+                    <Swiper 
+                        showsButtons={false} 
+                        showsPagination={true} 
+                        loop={true}
+                        index={this.state.selectedIndex} 
+                        onMomentumScrollEnd={(event, state, context) => {
+                            this.setState({
+                              selectedIndex: state.index,
+                            });
+                          }}
+                    >
+                        {
+                           categories.map((category) => {
+                               return (
+                                <View>
+                                    <Image 
+                                        source={{uri: category.imageUrl}} 
+                                        style={imageStyle} 
+                                    />
+                                    <Text style={categoryTitle}>{category.name}</Text>
+                                </View>
+                               )
+                               
+                           })
+                        }
                     </Swiper>
                 </View>
             </View>
@@ -35,11 +75,11 @@ const styles = StyleSheet.create({
     wrapper: {
         height: height * 0.3,
         backgroundColor: '#FFF',
-        margin: 10,
+        marginLeft: 10,
         shadowColor: '#121212',
         shadowOffset: { width: 0, height: 5},
         shadowOpacity: 0.2,
-        padding: 5,
+        padding: 0,
         paddingTop: 0
     },
 
@@ -48,23 +88,15 @@ const styles = StyleSheet.create({
         color: '#121212'
     },
 
+    categoryTitle: {
+        fontSize: 15,
+        color: '#121212',
+        textAlign: 'center'
+    },
+
     imageStyle: {
         height: imageHeight,
         width: imageWidth,
-        marginLeft: 50
+        alignSelf: 'center'
     },
-
-    imageStyleBo: {
-        height: 160,
-        width: 190,
-        marginLeft: 100,
-        marginBottom: 40
-    },
-
-    imageStyleCam: {
-        height: 150,
-        width: 160,
-        marginLeft: 120,
-        marginBottom: 40
-    }
 });
